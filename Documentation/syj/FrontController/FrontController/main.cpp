2db14d164836e5491a46655d110eefbc3d927d7e
Play0718 <835202990@qq.com>
2020-11-22 15:20:46 +0800

    update again

--- Documentation/syj/FrontController/FrontController/main.cpp
+++
@@ -1,9 +0,0 @@
-
-#include<iostream>
-#include"FrontController.h"
-using namespace std;
-int main() {
-	FrontController* frontController = new FrontController();
-	frontController->test();
-	return 0;
-}
--- Documentation/syj/command/command/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "command.h"
-using namespace std;
-int main() {
-	command* com = new command();
-	//²âÊÔº¯Êı
-	com->test();
-	return 0;
-}
